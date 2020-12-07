8b31f13dc4801d609c5c6079eb71c2eb486bdb01
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2020-12-07 20:15:09 +0700

    commit

--- bot_app.py
+++ bot_app.py
@@ -39,7 +39,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    message = event.message.text
+    if type(message) == str:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(message)
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage("Ew stickers...")
+        )
