adf48314092ddc4fa3831d1416e5935506dcaf97
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2020-12-12 01:18:18 +0700

    try to use placeholder

--- line_bot_app/source_handlers/user_handler/flex_message_templates/qotd_flex_template.py
+++ line_bot_app/source_handlers/user_handler/flex_message_templates/qotd_flex_template.py
@@ -3,31 +3,16 @@
 def get_qotd_flex_message(imageUrl="", quoteText="", quoteAuthor=""):
     return {
         "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "#Quote of the Day",
-                    "weight": "bold",
-                    "margin": "none",
-                    "size": "xl",
-                    "style": "italic",
-                    "position": "relative",
-                    "align": "center",
-                    "decoration": "underline",
-                    "color": "#2b2a27"
-                }
-            ],
-            "backgroundColor": "#ffd42b"
-        },
         "hero": {
             "type": "image",
-            "url": imageUrl,
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
             "size": "full",
             "aspectRatio": "20:13",
-            "aspectMode": "cover"
+            "aspectMode": "cover",
+            "action": {
+                "type": "uri",
+                "uri": "http://linecorp.com/"
+            }
         },
         "body": {
             "type": "box",
@@ -35,23 +20,103 @@ def get_qotd_flex_message(imageUrl="", quoteText="", quoteAuthor=""):
             "contents": [
                 {
                     "type": "text",
-                    "text": quoteText,
+                    "text": "Brown Cafe",
                     "weight": "bold",
-                    "size": "lg",
-                    "wrap": True,
-                    "align": "start",
-                    "adjustMode": "shrink-to-fit",
-                    "color": "#2b2a27"
+                    "size": "xl"
                 },
                 {
-                    "type": "text",
-                    "text": quoteAuthor,
-                    "margin": "sm",
-                    "size": "sm",
-                    "color": "#2b2a2a"
+                    "type": "box",
+                    "layout": "baseline",
+                    "margin": "md",
+                    "contents": [
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                        },
+                        {
+                            "type": "icon",
+                            "size": "sm",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                        },
+                        {
+                            "type": "text",
+                            "text": "4.0",
+                            "size": "sm",
+                            "color": "#999999",
+                            "margin": "md",
+                            "flex": 0
+                        }
+                    ]
+                },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Place",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 1
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                    "wrap": True,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Time",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 1
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "10:00 - 23:00",
+                                    "wrap": True,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                            ]
+                        }
+                    ]
                 }
-            ],
-            "backgroundColor": "#ffd42b"
+            ]
         },
         "footer": {
             "type": "box",
@@ -63,9 +128,19 @@ def get_qotd_flex_message(imageUrl="", quoteText="", quoteAuthor=""):
                     "style": "link",
                     "height": "sm",
                     "action": {
-                        "type": "message",
-                        "label": "Get Another Quote",
-                        "text": "qotd"
+                        "type": "uri",
+                        "label": "CALL",
+                        "uri": "https://linecorp.com"
+                    }
+                },
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                        "type": "uri",
+                        "label": "WEBSITE",
+                        "uri": "https://linecorp.com"
                     }
                 },
                 {
