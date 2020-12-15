dd6d78e857cba5d64a43c34d6b5bc9959d019bda
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2020-12-16 02:10:32 +0700

    fixed bug where temp is chnaged into string variabel type

--- line_bot_app/handlers_by_source_type/user_handler/flex_message_response/flex_message_templates/weather_carousel_template.py
+++ line_bot_app/handlers_by_source_type/user_handler/flex_message_response/flex_message_templates/weather_carousel_template.py
@@ -84,8 +84,8 @@ def get_weather_carousel_message(weatherDataHourly={}):
     timezoneOffset = weatherDataHourly["timezone_offset"]
     for data in weatherDataHourly["hourly"][:20:2]:
         time = datetime.utcfromtimestamp(data["dt"]+timezoneOffset).strftime('%H:%M')
-        temp = data["temp"] + "°C"
-        realFeelTemp = data["feels_like"] + "°C"
+        temp = str(data["temp"]) + "°C"
+        realFeelTemp = str(data["feels_like"]) + "°C"
         weatherDesc, weatherIcon = data["weather"][0]["description"], data["weather"][0]["icon"]
         bubbleContents.append(get_weather_bubble_message(time, weatherIcon, temp, realFeelTemp, weatherDesc))
 
